a65df399ac245b023d9eec6e6d6e8a80ff79b15c
chiara <12999292@itisrossi.vi.it>
2026-02-04 11:15:46 +0100

    esercizio classe

--- main.cpp
+++ main.cpp
@@ -1,4 +1,7 @@
 #include <iostream>
+#include <array>
+#include<algorithm>
+#include <vector>
 using namespace std;
 // TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 int main() {
@@ -8,7 +11,35 @@ int main() {
     int q = 0;
     int multipli = 0;
     bool negativi = false;
+    std::array<int, 10> arrayCpp = {20, 50, -10, -40, 80, -30, 70, 55, 75, 5};
+    std::sort(arrayCpp.begin(), arrayCpp.end());
+    std::vector vectorCpp = {20, 50, -10, -40, 80, -30, 70, 55, 75, 5};
+    vectorCpp.push_back(-100);
+    std::vector vectorCpp2(arrayCpp.begin(), arrayCpp.end());
 
+    std::sort(vectorCpp.begin(), vectorCpp.end());
+    std::reverse(vectorCpp.begin(), vectorCpp.end());
+    vectorCpp.pop_back();
+
+    //funzione anonima
+    auto risultato = std::find_if(vectorCpp.begin(), vectorCpp.end(),[](int x) {
+        if (x == 50)
+            return true;
+        return false;
+    });
+    //find_if fa un ciclo fino a end e dentro al parametri che gli passo c'è un numero fino alla quale deve arrivare
+    //sarebbe come if(x == 50) return true; return false;
+;
+    int numeroDaPassare;
+    std::cout<<"inserisci numero"<<std::endl;
+    std::cin>>numeroDaPassare;
+    //VOGLIO PASSARGLIIL N UMERO DA CERCARE
+    auto risultato = std::find_if(vectorCpp.begin(), vectorCpp.end(),[numeroDaPassare](int x) {
+        if (x == numeroDaPassare)
+            return true;
+        return false;
+    });
+    std::cout << *risultato << std::endl;
     //ordina il vettore
     for (int i = 0; i < 10; i++) {
         for (int j = 0; j < 10; j++) {
@@ -18,6 +49,10 @@ int main() {
                 numeri[j] = q;
             }
         }
+    }
+    for (int i = 0; i < vectorCpp.size();i++) {
+        std::cout<<arrayCpp[i]<<" ";
+        std::cout<<vectorCpp[i]<<" ";
     }
 
     //conta i multipli
